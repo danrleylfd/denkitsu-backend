e6ea324469d90c7548e129098508f540cea87b2d
Danrley <danrley.reply+github@gmail.com>
2025-04-19 19:52:57 -0300

    not user, id

--- src/app/views/linker/readMany.js
+++ src/app/views/linker/readMany.js
@@ -1,3 +1,4 @@
+const mongoose = require("../../../utils/database")
 const User = require("../../models/auth")
 const Linker = require("../../models/linker")
 
@@ -7,7 +8,7 @@ module.exports = async (req, res) => {
     console.log(`Id do usuário: ${id}`)
     const user = await User.findById(id)
     if (!user) return res.status(404).json({ error: "User not found/exist" })
-    const linkers = await Linker.find({ user: id }).sort("-createdAt").populate("user").exec()
+    const linkers = await Linker.find({ user: user._id }).sort("-createdAt").populate("user").exec()
     if (!linkers || linkers.length === 0) return res.status(404).json({ error: "No labels found" })
     return res.status(200).json(linkers)
   } catch (error) {
