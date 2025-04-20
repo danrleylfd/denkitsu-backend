dfb3ad59fe23ea67b7de00d0c3273e7d9f3f4ff9
Danrley <danrley.reply+github@gmail.com>
2025-04-20 19:48:48 -0300

    compare user._id n userID

--- src/app/views/account/editAccount.js
+++ src/app/views/account/editAccount.js
@@ -12,7 +12,8 @@ module.exports = async (req, res) => {
     if (!password || password.trim().length < 8) return res.status(422).json({ error: "password missing or too short." })
     let user = await User.findOne({ email: email.trim() }).select("+password")
     if (!user) return res.status(404).json({ error: "User not found/exist." })
-    if (user._id !== userID) return res.status(401).json({ error: "Invalid user." })
+    console.log("user._id, userID", user._id, userID)
+    if (user._id != userID) return res.status(401).json({ error: "Invalid user." })
     let avatarUrl
     if (!_avatarUrl || _avatarUrl.trim().length === 0) avatarUrl = user.avatarUrl
     const _password = await compare(password, user.password)
