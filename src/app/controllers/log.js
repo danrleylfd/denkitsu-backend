e9346939c6e502e777a989b7a3900c43ef8bc740
Danrley <danrley.reply+github@gmail.com>
2025-04-21 17:37:01 -0300

    Solve bugs and add log routes

--- src/app/controllers/log.js
+++ src/app/controllers/log.js
@@ -3,6 +3,6 @@ const routes = Router()
 
 const readAll = require("../views/log/readAll")
 
-routes.get("/", readAll)
+routes.get("/read_all", readAll)
 
 module.exports = (app) => app.use("/log", routes)
