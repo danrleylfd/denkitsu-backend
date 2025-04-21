950c9ea0d7d4b54a5074242ead4e3ca437e4d463
Danrley <danrley.reply+github@gmail.com>
2025-04-21 18:40:56 -0300

    Owner Auth

--- src/app/controllers/dashboard.js
+++ src/app/controllers/dashboard.js
@@ -1,5 +1,8 @@
 const { Router } = require("express")
+const ownerMiddleware = require("../middlewares/owner")
+
 const routes = Router()
+routes.use(ownerMiddleware)
 
 const readLogs = require("../views/dashboard/readLogs")
 
---
+++ src/app/middlewares/owner.js
@@ -0,0 +1,18 @@
+module.exports = (req, res, next) => {
+  try {
+    const authHeader = req.headers.authorization
+    if (!authHeader) return res.status(401).json({ error: "No token provided." })
+
+    const parts = authHeader.split(" ")
+    if (parts.length !== 2) return res.status(401).json({ error: "Token error." })
+
+    const [scheme, token] = parts
+    if (!/^Bearer$/i.test(scheme)) return res.status(401).json({ error: "Token malformatted." })
+
+    if (token !== "Denkitsu") return res.status(401).json({ error: "Token invalid." })
+    next()
+  } catch (error) {
+    console.error(error.message)
+    return res.status(500).json({ error: "Internal server error" })
+  }
+}
