049b696c1073be56117c5aac4194f211d7c3ee54
Danrley <danrley.reply+github@gmail.com>
2025-04-22 19:05:25 -0300

    solve path

--- src/app/middlewares/comment.js
+++ src/app/middlewares/comment.js
@@ -4,9 +4,7 @@ module.exports = (req, res, next) => {
   try {
     const { video: videoID } = req.params
     if (!videoID || videoID.trim().length !== 24) throw new Error("VIDEO_MISSING")
-    Video.findById(videoID).then((video) => {
-      if (!video) throw new Error("VIDEO_NOT_FOUND")
-    })
+    Video.findById(videoID).catch((_) => { throw new Error("VIDEO_NOT_FOUND") })
     next()
   } catch (error) {
     console.error(`[COMMENT_MIDDLEWARE] ${new Date().toISOString()} -`, { error: error.message, stack: error.stack })
