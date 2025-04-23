74c1117a592329ff04cfc36b1fb7dcbf694c0a23
Danrley <danrley.reply+github@gmail.com>
2025-04-23 15:58:26 -0300

    Solved object id

--- src/app/controllers/comment.js
+++ src/app/controllers/comment.js
@@ -6,17 +6,17 @@ const videoMiddleware = require("../middlewares/video")
 const routes = Router()
 routes.use(authMiddleware)
 routes.use(logMiddleware)
-routes.use(videoMiddleware)
+// routes.use(videoMiddleware)
 
 const addComment = require("../views/video/comments/addComment")
 const countComments = require("../views/video/comments/countComments")
 const delComment = require("../views/video/comments/delComment")
 
-routes.post("/:video", addComment)
+routes.post("/:video", videoMiddleware, addComment)
 
-routes.get("/:video", countComments)
+routes.get("/:video", videoMiddleware, countComments)
 
-routes.delete("/:video/:comment", delComment)
+routes.delete("/:video/:comment", videoMiddleware, delComment)
 
 const loadCommentRoutes = (app) => app.use("/comments", routes)
 
