713054349ebe6ec638bac8695d5363cf3a4886ee
Danrley <danrley.reply+github@gmail.com>
2025-04-23 12:10:27 -0300

    Solve edit account

--- src/app/views/account/editAccount.js
+++ src/app/views/account/editAccount.js
@@ -5,7 +5,17 @@ module.exports = async (req, res) => {
   try {
     const { userID } = req
     const { name, avatarUrl } = req.body
-    if (!name?.trim() && !avatarUrl?.trim()) throw new Error("NAME_OR_AVATAR_MISSING")
+    if (name?.length > 16) throw new Error("NAME_TOO_LONG")
+    if (avatarUrl?.trim()) {
+      try {
+        const url = new URL(avatarUrl)
+        const isValidImageUrl = /\.(jpg|jpeg|png|webp)$/i.test(url.pathname)
+        if (!isValidImageUrl) throw new Error("INVALID_IMAGE_URL")
+        if (avatarUrl.length > 255) throw new Error("AVATAR_URL_TOO_LONG")
+      } catch {
+        throw new Error("INVALID_URL")
+      }
+    }
     const user = await User.findById(userID)
     user.name = name?.trim() || user.name
     user.avatarUrl = avatarUrl?.trim() || user.avatarUrl
@@ -19,7 +29,9 @@ module.exports = async (req, res) => {
     console.error(`[EDIT_ACCOUNT] ${new Date().toISOString()} - `, { error: error.message, stack: error.stack })
     const defaultError = { status: 500, message: `[EDIT_ACCOUNT] ${new Date().toISOString()} - Internal server error` }
     const errorMessages = {
-      NAME_OR_AVATAR_MISSING: { status: 422, message: "name or avatarUrl is required" },
+      NAME_TOO_LONG: { status: 422, message: "name must be less than 16 characters" },
+      INVALID_URL: { status: 422, message: "avatarUrl must be a valid url" },
+      INVALID_IMAGE_URL: { status: 422, message: "avatarUrl must be a valid image url" }
     }
     const { status, message } = errorMessages[error.message] || defaultError
     return res.status(status).json({ code: error.message, error: message })
