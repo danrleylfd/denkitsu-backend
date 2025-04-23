073a3d950bd49b0677976836c0693627979102e3
Danrley <danrley.reply+github@gmail.com>
2025-04-23 15:56:42 -0300

    Solved object id

--- src/app/views/video/comments/countComments.js
+++ src/app/views/video/comments/countComments.js
@@ -4,7 +4,7 @@ const Video = require("../../../models/video")
 module.exports = async (req, res) => {
   try {
     const { video: videoID } = req.params
-    const [ video ] = await Video.aggregate([{ $match: { _id: ObjectId(videoID) } }, { $project: { commentsCount: { $size: "$comments" } } }])
+    const [ video ] = await Video.aggregate([{ $match: { _id: new ObjectId(videoID) } }, { $project: { commentsCount: { $size: "$comments" } } }])
     return res.status(200).json({ comments: video.commentsCount })
   } catch (error) {
     console.error(`[COUNT_COMMENTS] ${new Date().toISOString()} -`, { error: error.message, stack: error.stack })
