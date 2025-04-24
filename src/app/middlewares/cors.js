f32fc8bbead8b161eb5d334b90d76b24db34a423
Danrley <danrley.reply+github@gmail.com>
2025-04-24 11:55:04 -0300

    ajust cors

--- src/app/middlewares/cors.js
+++ src/app/middlewares/cors.js
@@ -2,8 +2,19 @@ const cors = require("cors")
 
 module.exports = cors({
   origin: (origin, callback) => {
+    console.log(`origin: ${origin}`)
     const allowedOrigins = [
-      process.env.HOST0, process.env.HOST1, process.env.HOST2, process.env.HOST3, process.env.HOST4, process.env.HOST5, process.env.HOST6
+      process.env.HOST0,
+      process.env.HOST1,
+      process.env.HOST2,
+      process.env.HOST3,
+      process.env.HOST4,
+      process.env.HOST5,
+      process.env.HOST6,
+      process.env.HOST7,
+      process.env.HOST8,
+      process.env.HOST9,
+      process.env.HOST10
     ]
     if (!origin || allowedOrigins.includes(origin)) callback(null, true)
     else callback(new Error("Not allowed by CORS"))
