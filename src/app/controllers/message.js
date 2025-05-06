4238c3f02d8d5fc18f75f6016b0da0aaaf89e364
Danrley <danrley.reply+github@gmail.com>
2025-05-06 09:42:17 -0300

    feat(controllers): add auth middleware to message routes
    Integrate the auth middleware into the message routes to ensure that only authenticated users can access these endpoints. This enhances security by preventing unauthorized access to message-related functionality.

--- src/app/controllers/message.js
+++ src/app/controllers/message.js
@@ -1,7 +1,9 @@
 const { Router } = require("express")
+const authMiddleware = require("../middlewares/auth")
 // const logMiddleware = require("../middlewares/log")
 
 const routes = Router()
+routes.use(authMiddleware)
 // routes.use(logMiddleware)
 
 const sendMessage = require("../views/message/sendMessage")
