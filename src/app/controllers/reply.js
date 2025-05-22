0cb0ac6a3918643932b1ac7cff09e84a933915fe
Danrley <danrley.reply+github@gmail.com>
2025-05-22 10:44:48 -0300

    feat(replies): add endpoint to list replies for a comment
    This commit introduces a new endpoint to fetch replies for a specific comment. The endpoint is accessible via a GET request to `/replys/:comment` and returns the replies sorted by creation date in descending order. This functionality enhances the ability to retrieve and display nested comments in the application.

--- src/app/controllers/reply.js
+++ src/app/controllers/reply.js
@@ -9,9 +9,14 @@ routes.use(authMiddleware)
 // routes.use(replyMiddleware)
 
 const replyComment = require("../views/video/comments/replyComment")
+
+const listReplies = require("../views/video/comments/listReplies")
+
 const delReply = require("../views/video/comments/delReply")
 
 routes.post("/:comment", replyComment)
+
+routes.get("/:comment", listReplies)
 
 routes.delete("/:reply", delReply)
 
