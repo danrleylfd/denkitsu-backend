5cfd46a1a3a0b5e0c8b79ea451635bc9e0189da2
danrleylfd <danrley.reply+github@gmail.com>
2025-06-27 16:58:44 -0300

    upgrade AI backend

--- src/utils/services/ai/index.js
+++ src/utils/services/ai/index.js
@@ -2,6 +2,7 @@ const axios = require("axios")
 const sysPrompt = require("../../prompts")
 
 const ask = async (llm = "openrouter", prompts, options = {}, aiKey = undefined) => {
+  console.log("llm:", llm)
   const apiKey = llm === "groq" ? process.env.GROQ_API_KEY : process.env.OPENROUTER_API_KEY
   const apiUrl = llm === "groq"? process.env.GROQ_API_URL : process.env.OPENROUTER_API_URL
   const aiAPI = axios.create({
