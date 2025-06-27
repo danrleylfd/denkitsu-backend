58e0335f1b1b845e56497531ba65ca99171e583e
danrleylfd <danrley.reply+github@gmail.com>
2025-06-27 17:06:12 -0300

    upgrade AI backend

--- src/utils/services/ai/index.js
+++ src/utils/services/ai/index.js
@@ -15,7 +15,7 @@ const ask = async (llm = "openrouter", prompts, options = {}, aiKey = undefined)
       messages: [sysPrompt, ...prompts]
     })
   } catch (error) {
-    console.error(`Error calling ${llm} API:`, error)
+    console.error(`Error calling ${llm} API:`, error.response.data)
     throw error
   }
 }
