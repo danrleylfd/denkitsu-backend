d4646cc01abe798af3f124d86a3ac162b33a7ded
danrleylfd <danrley.reply+github@gmail.com>
2025-06-27 17:15:12 -0300

    Fix bug AI service

--- src/utils/services/ai/index.js
+++ src/utils/services/ai/index.js
@@ -4,14 +4,15 @@ const sysPrompt = require("../../prompts")
 const ask = async (llm = "openrouter", prompts, options = {}, aiKey = undefined) => {
   const apiKey = llm === "groq" ? process.env.GROQ_API_KEY : aiKey ? aiKey : process.env.OPENROUTER_API_KEY
   const apiUrl = llm === "groq"? process.env.GROQ_API_URL : process.env.OPENROUTER_API_URL
-  console.log({ llm, apiUrl, apiKey })
+  const model = llm === "groq"? "deepseek-r1-distill-llama-70b" : options.model
+  console.log({ llm, apiKey, apiUrl, model })
   const aiAPI = axios.create({
     baseURL: apiUrl,
     headers: { "Content-Type": "application/json", Authorization: `Bearer ${apiKey}` }
   })
   try {
     return await aiAPI.post("/chat/completions", {
-      model: llm === "grok" ? "deepseek-r1-distill-llama-70b" : options.model,
+      model,
       messages: [sysPrompt, ...prompts]
     })
   } catch (error) {
