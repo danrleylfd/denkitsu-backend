a1ee6557cdbc4e624ba315f8c427e50ed9082c1d
danrleylfd <danrley.reply+github@gmail.com>
2025-08-10 12:22:43 -0300

    docs(duckduckgo): update tool description to include result format details

--- src/utils/api/duckduckgo.js
+++ src/utils/api/duckduckgo.js
@@ -45,7 +45,7 @@ const duckduckgoTool = {
   type: "function",
   function: {
     name: "duckduckgoTool",
-    description: "Use essa tool para fazer uma pesquisa no buscador Duckduckgo. Se um resumo direto (summary) for retornado, apresente-o primeiro. Sua resposta DEVE ser uma lista numerada(Não tabela) de resultados no formato `1. [Título do Site](URL)`. Após apresentar a lista, você DEVE perguntar ao usuário: 'Qual destes resultados você gostaria que eu acessasse para obter mais detalhes?'.",
+    description: "Use essa tool para fazer uma pesquisa no buscador Duckduckgo. Se um resumo direto (summary) for retornado, apresente-o primeiro. Sua resposta DEVE ser semelhante a um resultado de busca de um buscador, uma lista numerada(Não tabela) de resultados no formato `1. [Título do Site](URL)\nDescrição...`. Após apresentar a lista, você DEVE perguntar ao usuário: 'Qual destes resultados você gostaria que eu acessasse para obter mais detalhes?'.",
     parameters: {
       type: "object",
       properties: {
