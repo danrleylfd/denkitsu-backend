e84b5467e247f40de771045551d45dc05d75bf0c
danrleylfd <danrley.reply+github@gmail.com>
2025-09-06 12:05:02 -0300

    refactor(weather): remove deprecated weather API endpoints and validators
    The weather API endpoints and related validation logic have been deprecated and are no longer needed. This removes all associated files including controllers, views, and validators.

--- src/app/views/weather/getWeatherByCoordinates.js
+++
@@ -1,9 +0,0 @@
-const { getWeatherByCoordinates: getByCoordinates } = require("../../../utils/api/weather")
-
-const getWeatherByCoordinates = async (req, res) => {
-  const { lat, lon } = req.params
-  const { status, data } = await getByCoordinates(lat, lon)
-  return res.status(status).json(data)
-}
-
-module.exports = getWeatherByCoordinates
