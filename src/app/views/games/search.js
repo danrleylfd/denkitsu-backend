21e204f75d94301ef13d72a75a294096deef4348
danrleylfd <danrley.reply+github@gmail.com>
2025-09-06 11:02:24 -0300

    refactor: remove unused search games functionality
    The search endpoint and its validator were deleted as they're no longer needed in the codebase. This simplifies the project by removing unused code.

--- src/app/views/games/search.js
+++
@@ -1,9 +0,0 @@
-const { searchGames } = require("../../../utils/api/rawg")
-
-const search = async (req, res) => {
-  const { query } = req.query
-  const { status, data } = await searchGames(query)
-  return res.status(status).json(data)
-}
-
-module.exports = search
