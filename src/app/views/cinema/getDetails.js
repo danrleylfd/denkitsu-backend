9a8674a87257391c1c42d0878717b5ff4376de6d
danrleylfd <danrley.reply+github@gmail.com>
2025-09-06 10:59:31 -0300

    refactor: remove deprecated cinema module and related files
    The cinema module and its associated files (controllers, views, and validators) have been removed as they are no longer needed in the codebase. This cleanup helps maintain a more focused and streamlined project structure.

--- src/app/views/cinema/getDetails.js
+++
@@ -1,9 +0,0 @@
-const { getMediaDetails } = require("../../../utils/api/tmdb")
-
-const getDetails = async (req, res) => {
-  const { type, id } = req.params
-  const { status, data } = await getMediaDetails(type, id)
-  return res.status(status).json(data)
-}
-
-module.exports = getDetails
--- src/app/views/cinema/search.js
+++
@@ -1,9 +0,0 @@
-const { searchMedia } = require("../../../utils/api/tmdb")
-
-const search = async (req, res) => {
-  const { query } = req.query
-  const { status, data } = await searchMedia(query)
-  return res.status(status).json(data)
-}
-
-module.exports = search
